5f337b83c3e9334a6dc46f6aaba32e0ae3683b7f
109b29661 <91444896+109b29661@users.noreply.github.com>
2021-11-15 03:39:48 +0800

    Create Class05.java

---
+++ Class05.java
@@ -0,0 +1,23 @@
+class Caaa{
+	private int num;
+	public Caaa(int n) {
+	 num=n;
+	}
+	public int get() {
+		return num;
+	}
+}
+class Cbbb extends Caaa{
+	public Cbbb(int n) {
+		 super(n);
+		}
+	public void show() {
+		System.out.println("num="+get());
+	}
+}
+public class {
+public static void main(String args[]) {
+	Cbbb bb = new Cbbb(2);
+	bb.show();
+}
+}
